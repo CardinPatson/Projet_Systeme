d25a8248789b871eae79668be3eae13012b4c4c1
root <cardin.tiako@gmail.com>
2021-09-22 08:53:15 +0000

    initialisation

---
+++ projet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	int numero
+	char pilote
+ 	int vitesse	
+}voiture 
+//difffere sur le numero
+// 1 course = 300km
+int main(int argc , char **argv){
+	voiture voiture1
+
+	int nombre;
+	nombre = random();
+	printf("%ld" , nombre); 
+}
