97d42a819a63851de9d5c6b31d9d284f2746ec4c
Patson <74672498+CardinPatson@users.noreply.github.com>
2021-09-22 11:16:57 +0200

    Update projet.c

--- projet.c
+++ projet.c
@@ -9,9 +9,5 @@ typedef struct{
 //difffere sur le numero
 // 1 course = 300km
 int main(int argc , char **argv){
-	voiture voiture1
-
-	int nombre;
-	nombre = random();
-	printf("%ld" , nombre); 
+	
 }
