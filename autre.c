9ce54478258bafff3ec25e0895c5db09aa517623
Abdel <bellaalirachid@gmail.com>
2021-09-26 10:56:57 +0200

    Update autre.c

--- autre.c
+++ autre.c
@@ -9,7 +9,7 @@ int generateNumber(void);
 
 typedef struct {
         int numero;
-        
+
         int *temps_S1;
         int *temps_S2;
         int *temps_S3;
@@ -27,10 +27,11 @@ int main(void) {
 
     int compteur = 0; // pour savoir si malloc() ou realloc()
 
+    for (size_t a = 0; a < NOMBRE; a++)
+    {
+    tabStuctVoiture[a].tempsTotal = 0;
     
-    
-
-    while(tabStuctVoiture[0].tempsTotal <= 5400) {
+    while(tabStuctVoiture[a].tempsTotal <= 5400) {
 
         if (compteur == 0)
         {
@@ -43,6 +44,26 @@ int main(void) {
                 }
                 tabStuctVoiture[k].temps_S1[0] = generateNumber();
                 tabStuctVoiture[k].tempsTotal += tabStuctVoiture[k].temps_S1[0];
+
+
+                tabStuctVoiture[k].temps_S2 = calloc(1, sizeof(int));
+                if (tabStuctVoiture[k].temps_S2 == NULL) {
+                    perror("Problem calloc() !");
+                    exit(EXIT_FAILURE);
+                }
+                tabStuctVoiture[k].temps_S2[0] = generateNumber();
+                tabStuctVoiture[k].tempsTotal += tabStuctVoiture[k].temps_S2[0];
+
+
+
+                tabStuctVoiture[k].temps_S3 = calloc(1, sizeof(int));
+                if (tabStuctVoiture[k].temps_S3 == NULL) {
+                    perror("Problem calloc() !");
+                    exit(EXIT_FAILURE);
+                }
+                tabStuctVoiture[k].temps_S3[0] = generateNumber();
+                tabStuctVoiture[k].tempsTotal += tabStuctVoiture[k].temps_S3[0];
+
             }
             compteur++;
         }
@@ -58,20 +79,42 @@ int main(void) {
                 }
                 tabStuctVoiture[k].temps_S1[compteur] = generateNumber();
                 tabStuctVoiture[k].tempsTotal += tabStuctVoiture[k].temps_S1[compteur];
+
+
+                tabStuctVoiture[k].temps_S2 = realloc(tabStuctVoiture[k].temps_S2, (compteur + 1) * sizeof(int));
+                if (tabStuctVoiture[k].temps_S2 == NULL) {
+                    perror("Problem realloc() !");
+                    exit(EXIT_FAILURE);
+                }
+                tabStuctVoiture[k].temps_S2[compteur] = generateNumber();
+                tabStuctVoiture[k].tempsTotal += tabStuctVoiture[k].temps_S2[compteur];
+
+
+                tabStuctVoiture[k].temps_S3 = realloc(tabStuctVoiture[k].temps_S3, (compteur + 1) * sizeof(int));
+                if (tabStuctVoiture[k].temps_S3 == NULL) {
+                    perror("Problem realloc() !");
+                    exit(EXIT_FAILURE);
+                }
+                tabStuctVoiture[k].temps_S3[compteur] = generateNumber();
+                tabStuctVoiture[k].tempsTotal += tabStuctVoiture[k].temps_S3[compteur];
             }
             compteur++;
         }
     }
+    }
 
 
-    printf("%d\n", compteur);
-    printf("%d\n\n", tabStuctVoiture[0].tempsTotal);
 
     for(size_t i=0; i < NOMBRE; i++)
     {
+        printf("%d\n\n", tabStuctVoiture[i].tempsTotal);
+
         for(size_t j = 0; j < compteur; j++)
         {
         printf("%4d",tabStuctVoiture[i].temps_S1[j]);
+        printf("%4d",tabStuctVoiture[i].temps_S2[j]);
+        printf("%4d",tabStuctVoiture[i].temps_S3[j]);
+
         }
         printf("\n");
     }
