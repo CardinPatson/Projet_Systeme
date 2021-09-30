5c8055c1a67fb9850f258a1893b373a719f14712
Ikram <71373221+HE201916@users.noreply.github.com>
2021-09-30 22:19:15 +0200

    Update projet.c

--- projet.c
+++ projet.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h> 
+#include <unistd.h> 
 
 int generateNumberBetween25_40();
 
@@ -137,7 +141,45 @@ int main(int argc , char **argv)
 }
 
 
+int saveFile()
+{
+  int fd;
+  int rc;
+  char fileName[]="resultat.txt";
+  char data[]="voiture 44 : 350 sec";
+  mode_t mode = S_IRUSR | S_IWUSR;
 
+ 
+  if ((fd=open(fileName,O_CREAT,mode)) < 0)
+  {
+    perror("Erreur lors du Open pour créer le fichier");
+    return(0);
+  }
+
+  if (close(fd) < 0)
+  {
+    perror("Erreur lors du close.");
+    return(0);  
+  }
+
+  if ((fd=open(fileName,O_RDWR,mode)) < 0)
+  {
+    perror("Erreur lors du Open pour écrire dans le fichier");
+    return(0);  
+  }
+	//++ boucle pour parcourir le tableau des resultats et ecrire dans un fichier
+  if (-1 == (rc=write(fd, data, strlen(data))))
+  {
+    perror("Erreur lors de l\'écriture dans le fichier");
+    return(0);
+  }
+
+  if (0 != (rc=close(fd)))
+  {
+    perror("Erreur lors de la fermeture du fichier");
+    return(0);
+  }
+}
 
 int generateNumberBetween25_40(){
 
