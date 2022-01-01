29a227df5a40f01919fb935861c746de6df58bcd
Patson <74672498+CardinPatson@users.noreply.github.com>
2022-01-01 11:50:45 +0100

    Finale en fonction du nbre de tour

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #define MAX 40
 #define TEMPS_MAX_STAND 25
 #define TEMPS_MIN_STAND 12
+#define NOMBRE_TOURS_FINALE 45
 
 
 typedef struct {
@@ -27,7 +28,9 @@ typedef struct {
     unsigned int lap;
     unsigned int compteurStand;
     unsigned int isOut;
+    unsigned int nbre_tours;
     bool isFinished ;
+    
 } voiture;
 
 voiture *shared_memory;
@@ -53,7 +56,7 @@ unsigned int generateStandStop(void);
 bool goStand(unsigned int digit);
 void goOut(int i);
 int lancement(char **argv, const unsigned int* numeroVoiture);
-int finished(unsigned int tempsMaxCircuit);
+int finished(unsigned int tempsMaxCircuit , int nbre_tours_finale) ;
 void initBest(void) ;
 void define_session(int argc, char *argv[], unsigned int* numeroVoiture);
 bool savedFile(char *argv[]);
@@ -62,6 +65,7 @@ int modify(unsigned int numberArray[], char course[]);
 void prepa_qualifiedCars(void);
 void prepaClassementFinal(void);
 void removeNewLineChar(char *ptr);
+int finale(int i , unsigned int nbre_tours_max);
 
 
 unsigned int qualifiedCars[20];
@@ -96,6 +100,7 @@ int lancement(char **argv, const unsigned int* numeroVoiture)
     /**********************************************************
     *               Création des fils/voitures               *
     **********************************************************/
+
     for (int i = 0; i < current_session.total_cars; ++i)
     {
         initVoiture(i);
@@ -107,9 +112,19 @@ int lancement(char **argv, const unsigned int* numeroVoiture)
         }
         /********  le fils *********/
         if(pid == 0) {
-            shared_memory[i].id = numeroVoiture[i]; //attribution numéro pour chaque voiture
-            faireDesTours(i ,  current_session.session_time);
-            exit(EXIT_SUCCESS);
+            if (strcmp(argv[1], "RACE") == 0){
+                // printf("je suis dans le Race");
+                shared_memory[i].id = qualifiedCars[i];
+                shared_memory[i].nbre_tours = 0;
+                finale(i, NOMBRE_TOURS_FINALE);
+                exit(EXIT_SUCCESS);
+            }
+            else{
+                // printf("je ne ")
+                shared_memory[i].id = numeroVoiture[i]; //attribution numéro pour chaque voiture
+                faireDesTours(i ,  current_session.session_time);
+                exit(EXIT_SUCCESS);
+        }
         }
     }
     afficherTableau(current_session.session_time , argv);
@@ -181,6 +196,68 @@ int faireDesTours( int i , unsigned int tempsMaxCircuit ) {
     return 0;
 }
 
+int finale(int i , unsigned int nbre_tours_max){
+    
+    unsigned int tour_complet;
+    srand(time(NULL) + getpid());
+    while (shared_memory[i].nbre_tours <= nbre_tours_max && !shared_memory[i].isOut) //time pas dépassée
+    {
+        tour_complet = 0;
+        /*   ****       S1     ****     */
+        shared_memory[i].s1 = generateNumber();
+        if (shared_memory[i].s1 < shared_memory[20].s1) {
+            shared_memory[20].s1 = shared_memory[i].s1;
+        }
+        shared_memory[i].tempsTotal += shared_memory[i].s1;
+        tour_complet += shared_memory[i].s1;
+        
+
+        /*   ****       S2     ****     */
+        shared_memory[i].s2 = generateNumber();
+        if (shared_memory[i].s2 < shared_memory[20].s2) {
+            shared_memory[20].s2 = shared_memory[i].s2;
+        }
+        shared_memory[i].tempsTotal += shared_memory[i].s2;
+        tour_complet += shared_memory[i].s2;
+
+
+        /**STAND*/
+        shared_memory[i].s3 = generateNumber();
+        //si dernier digit == 9 ==> go stand secteur3 + generer le temps sup
+        if (goStand(shared_memory[i].s2)) {
+            unsigned int timeSupplementaire ;
+            shared_memory[i].compteurStand++;
+            timeSupplementaire = generateStandStop();
+            shared_memory[i].s3 += timeSupplementaire;
+            goOut(i);
+        }
+        sleep(1);
+
+        /*   ****       S3     ****     */
+        if (shared_memory[i].s3 < shared_memory[20].s3) {
+            shared_memory[20].s3 = shared_memory[i].s3;
+        }
+        shared_memory[i].tempsTotal += shared_memory[i].s3;
+        tour_complet += shared_memory[i].s3;
+        
+        /* *************************************** */
+
+        /*   ****       Best Time Circuit de toutes les voitures     ****     */
+        if (tour_complet < shared_memory[20].best_Circuit) {
+            shared_memory[20].best_Circuit = tour_complet;
+        }
+
+        /*   ****       Best Time Circuit de la voiture actuelle     ****     */
+        if (tour_complet < shared_memory[i].best_Circuit) {
+            shared_memory[i].best_Circuit = tour_complet;
+        }
+        usleep(80);
+        /* *************************************** */
+        shared_memory[i].nbre_tours++;
+    }
+
+    return 0 ;
+}
 
 unsigned int generateNumber(void) {
     return rand() * clock()%(MAX-MIN+1)+MIN;
@@ -251,17 +328,18 @@ void afficherTableau(unsigned int tempsMaxCircuit , char **argv) {
         printf(" |     ID   |      s1     |      s2     |      s3     |     Tour    |     LAP     |   Stand  |\n");
         printf(" |===========================================================================================|\n");
         for (int i = 0; i < current_session.total_cars; i++){
-            printf(" |     %2d   |    %5d    |    %5d    |    %5d    |    %5d    |    %5d    |    %2d    | %5d\n", \
+            printf(" |     %2d   |    %5d    |    %5d    |    %5d    |    %5d    |    %5d    |    %2d    | %5d    | %2d  \n", \
                     copyTableau[i].id, \
                     copyTableau[i].s1, copyTableau[i].s2, copyTableau[i].s3, \
                     copyTableau[i].best_Circuit,\
                     copyTableau[i].lap, \
-                    copyTableau[i].compteurStand, copyTableau[i].tempsTotal);
+                    copyTableau[i].compteurStand, copyTableau[i].tempsTotal, \
+                    copyTableau[i].nbre_tours);
         }
         printf(" =============================================================================================\n\n");
         printf("bs1: %d, bs2: %d, bs3: %d et b_circuit %d\n", copyTableau[20].s1, copyTableau[20].s2, copyTableau[20].s3, copyTableau[20].best_Circuit);
         //si toutes les voitures on terminer la course
-        if(finished(tempsMaxCircuit)){
+        if(finished(tempsMaxCircuit , NOMBRE_TOURS_FINALE)){
             savedFile(argv);
             break;
         }
@@ -269,9 +347,12 @@ void afficherTableau(unsigned int tempsMaxCircuit , char **argv) {
     }
 }
 
-int finished(unsigned int tempsMaxCircuit) {
+int finished(unsigned int tempsMaxCircuit , int nbre_tours_finale) {
     for (int i = 0; i < current_session.total_cars; ++i) {
         if (shared_memory[i].tempsTotal >= tempsMaxCircuit) {
+            return 1;
+        }
+        else if (shared_memory[i].nbre_tours >= nbre_tours_finale) {
             return 1;
         }
     }
@@ -290,14 +371,14 @@ void define_session(int argc, char *argv[], unsigned int* numeroVoiture){
         perror("Invalid Parameter");
         exit(-1);
     }
-    else if (!strcmp(argv[1], "RACE") && argc == 2){
-        perror("Invalid Parameter");
-        exit(-1);
-    }
-    else if (strcmp(argv[1], "RACE") != 0 && argc == 3){
-        perror("Invalid Parameter");
-        exit(-1);
-    }
+    // else if (!strcmp(argv[1], "RACE") && argc == 2){
+    //     perror("Invalid Parameter");
+    //     exit(-1);
+    // }
+    // else if (strcmp(argv[1], "RACE") != 0 && argc == 3){
+    //     perror("Invalid Parameter");
+    //     exit(-1);
+    // }
     sprintf(current_session.file_name, "%s.txt", argv[1]);  // On définit le nom du fichier à enregistrer
     // Paramètreage de la session en fonction des arguments
     // == 0 -> exactement meme que !
@@ -362,10 +443,12 @@ void define_session(int argc, char *argv[], unsigned int* numeroVoiture){
         prepa_qualifiedCars();
         prepaClassementFinal();
 
+
+        /**Test pour le tableau qualified cars*/
         for (int i = 0; i < 20; ++i) {
             printf("%d\n", qualifiedCars[i]);
         }
-        exit(1);
+       
     }
     else{
         perror("error: invalid first argument, first argument must be in [P1, P2, P3, Q1, Q2, Q3, RACE]");
@@ -484,9 +567,10 @@ void prepaClassementFinal(void) {
 #define NUMBER_OF_STRING 3
 #define MAX_STRING_SIZE 14
 
+    
     char arr[NUMBER_OF_STRING][MAX_STRING_SIZE] = {"./data/Q3.txt", "./data/Q2.txt", "./data/Q1.txt"};
 
-    FILE *fichier = fopen("./data/CLASSEMENT", "a+");
+    FILE *fichier = fopen("./data/CLASSEMENT.txt", "a+");
     if (fichier == NULL) {
         perror("fopen() failed !");
         exit(EXIT_FAILURE);
@@ -503,16 +587,19 @@ void prepaClassementFinal(void) {
             strtok(line,"\n");
 
             if(i == 0) {
+                //Recuperer les 10 premieres lignes dans le fichier Q3 et mettre dans line 
                 if(j >= 0 && j <= 10) {
                     fprintf(fichier, "%s\n", line);
                 }
             }
             if(i == 1){
+                //Recuperer les lignes 11 à 15 de Q2
                 if(j > 10 && j <= 15) {
                     fprintf(fichier, "%s\n", line);
                 }
             }
             if(i == 2){
+                //Récuperer les ligne 16 à 20 de Q3
                 if(j > 15 && j <= 20) {
                     fprintf(fichier, "%s\n", line);
                 }
@@ -522,7 +609,7 @@ void prepaClassementFinal(void) {
     }
     for(int i = 0 ; i<20 ; i++ ){
         fscanf(fichier, "%d" , qualifiedCars[i]);
-        printf("qualified cars -- %d" , qualifiedCars[i]);
+     
     }
     fclose(fichier);
 }
